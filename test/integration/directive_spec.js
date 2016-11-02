794284cfc55532733b2fe5c35a4914fea420f486
Richard Clayton <rlc.blacksun@gmail.com>
2016-11-01 17:02:14 -0700

    Adds integration tests and fixes an error with Watch

--- test/integration/directive_spec.js
+++ test/integration/directive_spec.js
@@ -15,12 +15,12 @@ var chai = require('chai'),
   ConsulDirective = require('../../lib/directive');
 
 describe('Consul Directive', function() {
-  describe('Once', function(){
-    var fooBarValue = 'test-testy-test';
+  describe('when retrieving a value once', function(){
+    var fooValue = 'test-testy-test';
     var directive = new ConsulDirective();
     beforeEach(function(done){
-      this.timeout(15000);
-      consul.kv.set('foo', fooBarValue, function(err){
+      this.timeout(5000);
+      consul.kv.set('foo', fooValue, function(err){
         done(err);
       });
     });
@@ -37,8 +37,46 @@ describe('Consul Directive', function() {
         expect(directiveContext).to.be.an.instanceOf(DirectiveContext);
         var manager = directiveContext.value;
         var kv = manager.value;
-        expect(kv.Value).to.eq(fooBarValue);
+        expect(kv.Value).to.eq(fooValue);
         next();
+      });
+    });
+  });
+
+  describe('when watching a value', function(){
+    var barValue = '1';
+    var barChangedValue = '2';
+    var directive = new ConsulDirective();
+    beforeEach(function(done){
+      this.timeout(2000);
+      consul.kv.set('bar', barValue, function(err){
+        done(err);
+      });
+    });
+
+    it('should notify listeners of updates', function(next){
+      this.timeout(2000);
+      var context = new DirectiveContext(
+        'consul', 
+        'kv.get?key=bar&mode=watch&wait=1s', 
+        [{ field: 'bar' }]
+      );
+      var callback = sinon.spy();
+      directive.handle(context, {}, {}, function(err, directiveContext){
+        expect(err).to.be.null;
+        expect(directiveContext).to.be.an.instanceOf(DirectiveContext);
+        var manager = directiveContext.value;
+        expect(manager.value.Value).to.eq(barValue);
+        var changeListener = sinon.spy();
+        manager.on('change', changeListener);
+        consul.kv.set('bar', barChangedValue, function(err){
+          expect(err).to.be.undefined;
+          setTimeout(function(){
+            expect(changeListener).to.be.called;
+            expect(manager.value.Value).to.eq(barChangedValue);
+            next();
+          }, 1500);
+        });
       });
     });
   });
